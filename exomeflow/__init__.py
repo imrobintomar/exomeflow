36fb5187675317748f74d94fe88556fc587c9ff8
Robin Tomar <itsrobintomar@gmail.com>
2026-05-13 12:20:49 +0530

    v1.0.9: fix author name to Robin Kumar

--- exomeflow/__init__.py
+++ exomeflow/__init__.py
@@ -4,6 +4,6 @@ ExomeFlow — Whole Exome Sequencing analysis pipeline.
 Author: Robin Kumar, AIIMS New Delhi
 """
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 __author__ = "Robin Kumar"
 __email__ = "robin@aiims.ac.in"
